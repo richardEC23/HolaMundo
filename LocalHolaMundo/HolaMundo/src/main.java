1fe4a3b92354fc39bff496183b627354354e7b2b
User <User@User-PC>
2019-03-24 23:31:34 -0300

    codigo de mi primer programa Hola Mundo

---
+++ LocalHolaMundo/HolaMundo/src/main.java
@@ -0,0 +1,23 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo!");
+		int a;
+		a=10;
+		int b=2;
+		int c=0;
+		System.out.println("La variable a vale: "+a+" Y la b vale: "+b);
+		c=a*b;
+		a=c;
+		System.out.println("Ahora la variable a vale: "+a+" Y la b vale: "+b);
+		
+		if (a<b) {
+			System.out.println(a+" es mas pequeña que" + b);
+		} else {
+			System.out.println("b es mas grande");
+		}
+	}
+
+}
